e60b085a8330b8ea19e678da04601cae8b2837fc
Robert Daraz <rdaraz@deloittece.com>
2019-08-24 17:21:28 +0200

    Users-list component

---
+++ src/app/module/users.ts
@@ -0,0 +1,18 @@
+// eslint-disable-next-line import/prefer-default-export
+export const users = [
+  {
+    lp: 1,
+    nick: 'Morgoth',
+    points: 20,
+  },
+  {
+    lp: 2,
+    nick: 'Beleg',
+    points: 15,
+  },
+  {
+    lp: 3,
+    nick: 'Grubby',
+    points: 10,
+  },
+];
