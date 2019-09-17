066e6a636c48fc0289788ff17b3a6d5e19d5afd1
Robert Daraz <rdaraz@deloittece.com>
2019-09-17 15:33:30 +0200

    Design for server errors

--- server/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const app = express();
-app.use(express.static('public/styles'));
-app.use(express.static('public/views'));
-app.use(express.static('public/images'));
-
-app.listen(8000);
